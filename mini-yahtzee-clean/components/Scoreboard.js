09a029372e04fe1e5a51db65af647bcb3d3afd54
Jamiiiiii <jami34@windowslive.com>
2025-06-06 13:49:13 +0300

    updated dependecies.

--- mini-yahtzee-clean/components/Scoreboard.js
+++ mini-yahtzee-clean/components/Scoreboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import styles from '../components/styles/style';
+import styles from './styles/style';
 
 export default Footer = () => {
   return (
